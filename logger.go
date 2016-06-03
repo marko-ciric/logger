23d9dd27c26d325380eb3a2d04d67887845f0de5
Marko Ciric <ciric.marko@gmail.com>
2016-06-03 13:47:45 +0200

    Added formatting string functions.

--- logger.go
+++ logger.go
@@ -29,49 +29,82 @@ func GetLog(level logging.Level) *logging.Logger {
 
 	// Only errors and more severe messages should be sent to backend
 	backendLeveled := logging.AddModuleLevel(backend)
-	backendLeveled.SetLevel(level, "")
+	backendLeveled.SetLevel(level|logging.ERROR, "")
 
 	// Set the backends to be used.
 	logging.SetBackend(backendLeveled, backendFormatter)
+
 	return log
 }
 
-// Fatal is equivalent to l.Critical followed by a call to os.Exit(1).
-func Fatal(format string, args ...interface{}) {
+// Fatalf is equivalent to l.Critical followed by a call to os.Exit(1).
+func Fatalf(format string, args ...interface{}) {
 	Log.Fatalf(format, args)
 }
 
-// Panic is equivalent to l.Critical followed by a call to panic().
-func Panic(format string, args ...interface{}) {
+// Panicf is equivalent to l.Critical followed by a call to panic().
+func Panicf(format string, args ...interface{}) {
 	Log.Panicf(format, args)
 }
 
-// Critical logs a message using CRITICAL as log level.
-func Critical(format string, args ...interface{}) {
+// Criticalf logs a message using CRITICAL as log level.
+func Criticalf(format string, args ...interface{}) {
 	Log.Criticalf(format, args)
 }
 
-// Error logs a message using ERROR as log level.
-func Error(format string, args ...interface{}) {
+// Errorf logs a message using ERROR as log level.
+func Errorf(format string, args ...interface{}) {
 	Log.Errorf(format, args)
 }
 
-// Warning logs a message using WARNING as log level.
-func Warning(format string, args ...interface{}) {
+// Warningf logs a message using WARNING as log level.
+func Warningf(format string, args ...interface{}) {
 	Log.Warningf(format, args)
 }
 
-// Notice logs a message using NOTICE as log level.
-func Notice(format string, args ...interface{}) {
+// Noticef logs a message using NOTICE as log level.
+func Noticef(format string, args ...interface{}) {
 	Log.Noticef(format, args)
 }
 
-// Info logs a message using INFO as log level.
-func Info(format string, args ...interface{}) {
+// Infof logs a message using INFO as log level.
+func Infof(format string, args ...interface{}) {
 	Log.Infof(format, args)
 }
 
-// Debug logs a message using DEBUG as log level.
-func Debug(format string, args ...interface{}) {
+// Debugf logs a message using DEBUG as log level.
+func Debugf(format string, args ...interface{}) {
 	Log.Debugf(format, args)
+}
+
+func Fatal(args ...interface{}) {
+	Log.Fatal(args)
+}
+
+func Panic(args ...interface{}) {
+	Log.Panic(args)
+}
+
+func Critical(args ...interface{}) {
+	Log.Critical(args)
+}
+
+func Error(args ...interface{}) {
+	Log.Error(args)
+}
+
+func Warning(args ...interface{}) {
+	Log.Warning(args)
+}
+
+func Notice(args ...interface{}) {
+	Log.Notice(args)
+}
+
+func Info(args ...interface{}) {
+	Log.Info(args)
+}
+
+func Debug(args ...interface{}) {
+	Log.Debug(args)
 }
